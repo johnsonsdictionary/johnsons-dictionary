17d340582a6997a1c72b7cfc0e8df3d4eb802b98
Michael Lavelle <714044+michaellavelle@users.noreply.github.com>
2023-03-15 17:07:26 +0000

    Adding another batch of preterites and participles which are referenced by verb entries but which do not have entires of their own

--- omitted-preterites-and-participles/src/test/java/io/github/johnsonsdictionary/omittedpreteritesandparticiples/OmittedPreteritesAndParticiplesTest.java
+++ omitted-preterites-and-participles/src/test/java/io/github/johnsonsdictionary/omittedpreteritesandparticiples/OmittedPreteritesAndParticiplesTest.java
@@ -7,6 +7,6 @@ public class OmittedPreteritesAndParticiplesTest {
 
     @Test
     public void testOmittedPreteritesAndParticiplesLoad() {
-        Assertions.assertEquals(32, OmittedPreteritesAndParticiples.ALL_OMITTED_PRETERITES_AND_PARTICIPLES.keySet().size());
+        Assertions.assertEquals(83, OmittedPreteritesAndParticiples.ALL_OMITTED_PRETERITES_AND_PARTICIPLES.keySet().size());
     }
 }
